c8653a0cca6fb94bc16fb324388b5e528df1c7e9
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-01 09:11:31 +0300

    add setBounds

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
