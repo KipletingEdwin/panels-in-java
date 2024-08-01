4781fb347ddff0e9a501e7b6dcdeed413b9a01ba
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-01 10:23:15 +0300

    add an image Icon

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,16 @@ public class Main {
 
         //JPanel = a GUI component that functions as a container to hold other components
 
+        ImageIcon image = new ImageIcon("javaPng.png");
+
+        JLabel label = new JLabel();
+        label.setText("Hi");
+        label.setIcon(image);
+
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,250,250);
+
 
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.blue);
@@ -19,15 +26,18 @@ public class Main {
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0,250,500,250);
+        greenPanel.setLayout(new BorderLayout());
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(750,750);
         frame.setVisible(true);
+        greenPanel.add(label);
         frame.add(redPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
+
 
     }
 }
