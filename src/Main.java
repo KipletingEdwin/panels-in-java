ca5ddba64f4b3fdae198805413560bbfba67de30
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-02 08:22:41 +0300

    add setBounds to the label

--- src/Main.java
+++ src/Main.java
@@ -15,27 +15,30 @@ public class Main {
         label.setIcon(image);
         label.setVerticalAlignment(JLabel.TOP);
         label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(0,0,75,75);
 
         JPanel redPanel = new JPanel();
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,250,250);
+        redPanel.setLayout(null);
 
 
         JPanel bluePanel = new JPanel();
         bluePanel.setBackground(Color.blue);
         bluePanel.setBounds(250,0,250,250);
+        bluePanel.setLayout(null);
 
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.green);
         greenPanel.setBounds(0,250,500,250);
-        greenPanel.setLayout(new BorderLayout());
+        greenPanel.setLayout(null);
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(750,750);
         frame.setVisible(true);
-        greenPanel.add(label);
+        redPanel.add(label);
         frame.add(redPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
